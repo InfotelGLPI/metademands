ea821773b178dec33221dedb48a72afa9828e6a6
tsmr <thetsmr@gmail.com>
2019-07-16 12:18:24 +0200

    Fix security break

--- ajax/addnewvalue.php
+++ ajax/addnewvalue.php
@@ -28,9 +28,10 @@
  */
 
 include ('../../../inc/includes.php');
+header("Content-Type: text/html; charset=UTF-8");
 Html::header_nocache();
 
-Session::checkLoginUser();
+Session::checkRight("plugin_metademands", UPDATE);
 
 switch ($_POST['action']) {
    case 'add':
