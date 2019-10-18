0120a0c5669c7a3262e5ece4861b0af3c11924ef
keha35 <kevin.hainry@infotel.com>
2019-10-18 10:08:17 +0200

    Add link field type

--- inc/metademand.class.php
+++ inc/metademand.class.php
@@ -1052,6 +1052,12 @@ class PluginMetademandsMetademand extends CommonDropdown {
                   }
                }
                break;
+            case 'link':
+               if (strpos($field['value'],'http://') !== 0 && strpos($field['value'],'https://') !== 0 ){
+                  $field['value'] = "http://".$field['value'];
+               }
+               $result['content'] .= $field['label'] . ' :<a href="'.$field['value'].'" data-mce-href="'.$field['value'].'" > ' . $field['value'].'</a>';
+               break;
             case 'text':
                $result['content'] .= $field['label'] . ' : ' . $field['value'];
                break;
