3b49f682e6d6ad1166fad1958af3ce28ce6bd81e
keha35 <kevin.hainry@infotel.com>
2019-10-18 10:12:27 +0200

    Update version number for release

--- setup.php
+++ setup.php
@@ -27,7 +27,7 @@
  --------------------------------------------------------------------------
  */
 
-define('PLUGIN_METADEMANDS_VERSION', '2.6.0');
+define('PLUGIN_METADEMANDS_VERSION', '2.6.1');
 
 // Init the hooks of the plugins -Needed
 function plugin_init_metademands() {
