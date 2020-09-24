bcbd5ae3ae1bc988bc760615b3bbb66396f5c7a3
ales-infotel <alban.lesellier@infotel.com>
2020-09-24 17:23:52 +0200

    update unique index on basketline

--- install/sql/update-2.6.6.sql
+++ install/sql/update-2.6.6.sql
@@ -3,3 +3,7 @@ ALTER TABLE `glpi_plugin_metademands_fields` CHANGE `plugin_metademands_tasks_id
 ALTER TABLE `glpi_plugin_metademands_tickets_metademands` ADD `tickettemplates_id` INT(11) NOT NULL DEFAULT '0';
 ALTER TABLE `glpi_plugin_metademands_fields` ADD `max_upload` INT(11) NOT NULL DEFAULT 0;
 ALTER TABLE `glpi_plugin_metademands_fields` ADD `regex` VARCHAR(255) NOT NULL DEFAULT '';
+
+ALTER TABLE glpi_plugin_metademands_basketlines
+  DROP INDEX unicity;
+CREATE UNIQUE INDEX `unicity` ON `glpi_plugin_metademands_basketlines` (`plugin_metademands_metademands_id`,`plugin_metademands_fields_id`,`line`,`name`);
