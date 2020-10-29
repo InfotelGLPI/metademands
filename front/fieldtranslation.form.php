d5f83475dd1fec9e4bb1bdd4a2dba648c84cc86e
ales-infotel <53440122+ales-infotel@users.noreply.github.com>
2020-10-29 17:54:04 +0100

    Update fieldtranslation.form.php

--- front/fieldtranslation.form.php
+++ front/fieldtranslation.form.php
@@ -2,28 +2,27 @@
 
 /*
  -------------------------------------------------------------------------
- Servicecatalog plugin for GLPI
- Copyright (C) 2003-2019 by the Servicecatalog Development Team.
+ Metademands plugin for GLPI
+ Copyright (C) 2003-2019 by the Metademands Development Team.
 
- https://forge.indepnet.net/projects/servicecatalog
  -------------------------------------------------------------------------
 
  LICENSE
 
- This file is part of Servicecatalog.
+ This file is part of Metademands.
 
- Servicecatalog is free software; you can redistribute it and/or modify
+ Metademands is free software; you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
  the Free Software Foundation; either version 2 of the License, or
  (at your option) any later version.
 
- Servicecatalog is distributed in the hope that it will be useful,
+ Metademands is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  GNU General Public License for more details.
 
  You should have received a copy of the GNU General Public License
- along with Servicecatalog. If not, see <http://www.gnu.org/licenses/>.
+ along with Metademands. If not, see <http://www.gnu.org/licenses/>.
  --------------------------------------------------------------------------
  */
 
