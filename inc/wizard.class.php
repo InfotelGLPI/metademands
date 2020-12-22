d96f3ec97bd061b7b025e96cd57db8a6c0c315de
ales-infotel <alban.lesellier@infotel.com>
2020-12-22 16:46:16 +0100

    fix isset default value

--- inc/wizard.class.php
+++ inc/wizard.class.php
@@ -1109,9 +1109,14 @@ class PluginMetademandsWizard extends CommonDBTM {
                            $check_value    = array_flip($check_value);
                            foreach ($default_values as $k => $v) {
                               if ($v == 1) {
-                                 $idc    = $check_value[$k];
-                                 $idv    = $hidden_link[$idc];
-                                 $script .= " $('[id-field =\"field" . $idv . "\"]').show();";
+                                 if(isset($check_value[$k])){
+                                    $idc    = $check_value[$k];
+                                    if(isset($hidden_link[$idc])){
+                                       $idv    = $hidden_link[$idc];
+                                       $script .= " $('[id-field =\"field" . $idv . "\"]').show();";
+                                    }
+                                 }
+
                               }
                            }
                         }
@@ -1186,9 +1191,14 @@ class PluginMetademandsWizard extends CommonDBTM {
                            $check_value    = array_flip($check_value);
                            foreach ($default_values as $k => $v) {
                               if ($v == 1) {
-                                 $idc    = $check_value[$k];
-                                 $idv    = $hidden_link[$idc];
-                                 $script .= " $('[id-field =\"field" . $idv . "\"]').show();";
+                                 if(isset($check_value[$k])){
+                                    $idc    = $check_value[$k];
+                                    if(isset($hidden_link[$idc])){
+                                       $idv    = $hidden_link[$idc];
+                                       $script .= " $('[id-field =\"field" . $idv . "\"]').show();";
+                                    }
+                                 }
+
                               }
                            }
                         }
