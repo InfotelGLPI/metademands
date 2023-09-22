072424442b392a41b49bcfdee400ae6f6a7ef675
Thetsmr <thetsmr@gmail.com>
2023-09-22 10:22:41 +0200

    add sql files

---
+++ install/sql/update-3.3.4.sql
@@ -0,0 +1,2 @@
+ALTER TABLE `glpi_plugin_metademands_tickettasks` ADD `entities_id` int unsigned NOT NULL DEFAULT '0';
+ALTER TABLE `glpi_plugin_metademands_conditions` CHANGE `order` `order` INT NOT NULL DEFAULT '0';
