b074eb173866db5a263acaa3afd77e1bea47b31f
Thetsmr <thetsmr@gmail.com>
2024-01-28 12:35:23 +0100

    Min fixes

--- inc/field.class.php
+++ inc/field.class.php
@@ -2820,7 +2820,9 @@ JAVASCRIPT
             }
             if (is_array($input) && !empty($input)) {
                 foreach ($input as &$value) {
-                    $value = urldecode($value);
+                    if ($value != null) {
+                        $value = urldecode($value);
+                    }
                 }
             }
         }
