ef978f51fbdb823073b8ad6a8644e7511a741e8d
Thetsmr <thetsmr@gmail.com>
2024-03-13 14:47:28 +0100

    Bump version

--- setup.php
+++ setup.php
@@ -27,7 +27,7 @@
  --------------------------------------------------------------------------
  */
 
-define('PLUGIN_METADEMANDS_VERSION', '3.3.9');
+define('PLUGIN_METADEMANDS_VERSION', '3.3.10');
 
 if (!defined("PLUGIN_METADEMANDS_DIR")) {
     define("PLUGIN_METADEMANDS_DIR", Plugin::getPhpDir("metademands"));
