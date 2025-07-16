0a4f91ae6d50b04e8e0bdde6ce5b6f9e085214f2
Elric <elricchampenois@gmail.com>
2025-07-16 14:30:17 +0200

    add option for urgency

--- inc/typefields/dropdownmeta.class.php
+++ inc/typefields/dropdownmeta.class.php
@@ -1479,14 +1479,18 @@ JAVASCRIPT
         $field = new PluginMetademandsFieldOption();
         $existing_options = $field->find(["plugin_metademands_fields_id" => $params["plugin_metademands_fields_id"]]);
         $already_used = [];
+	    $name = "check_value";
 
         switch ($params["item"]) {
+            case 'urgency':
+	            Ticket::dropdownUrgency(["name" => $name,'value' =>$params['check_value']]);
+
+                break;
             case 'ITILCategory_Metademands':
                 $metademand = new PluginMetademandsMetademand();
                 $metademand->getFromDB($params["plugin_metademands_metademands_id"]);
                 $values = json_decode($metademand->fields['itilcategories_id']);
 
-                $name = "check_value";
                 $opt = [
                     'name' => $name,
                     'right' => 'all',
@@ -1519,7 +1523,6 @@ JAVASCRIPT
                     //               if ($params['value'] == 'group') {
                     //                  $name = "check_value";// TODO : HS POUR LES GROUPES CAR rajout un RAND dans le dropdownname
                     //               } else {
-                    $name = "check_value";
                     //               }
                     $params['item']::Dropdown([
                         "name" => $name,
@@ -1562,6 +1565,9 @@ JAVASCRIPT
             echo __('Not null value', 'metademands');
         } else {
             switch ($params["item"]) {
+	            case 'urgency':
+		            echo CommonITILObject::getUrgencyName($params['check_value']);
+		            break;
                 case 'ITILCategory_Metademands':
 
                     $pass = false;
