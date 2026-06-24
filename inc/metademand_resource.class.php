76ff8bdb6d49828237b24a8842cfc75eba05fd91
a_evrard <axel.evrard@infotel.com>
2026-06-24 11:53:04 +0200

    add description on new resource v10

--- inc/metademand_resource.class.php
+++ inc/metademand_resource.class.php
@@ -301,6 +301,12 @@ class PluginMetademandsMetademand_Resource extends CommonDBTM {
       $content .= "<td>" . __("Resource manager", "resources") . "</td>";
       $content .= "<td>" . getUserName($resource->fields['users_id'], 0, true). "</td>";
       $content .= "</tr>";
+      if (!empty($resource->fields['comment'])) {
+          $content .= "<tr>";
+          $content .= "<td>" . __("Description") . "</td>";
+          $content .= "<td>" . $resource->fields['comment'] . "</td>";
+          $content .= "</tr>";
+      }
 
       return $content;
 
