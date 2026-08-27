c19810c0315e17c1d547ebe2fdf5978f9391d7f8
Thetsmr <thetsmr@gmail.com>
2026-08-27 12:03:30 +0200

    Bump version

--- setup.php
+++ setup.php
@@ -59,7 +59,7 @@ use GlpiPlugin\Resources\Resource;
 
 use function Safe\define;
 
-define('PLUGIN_METADEMANDS_VERSION', '3.6.5');
+define('PLUGIN_METADEMANDS_VERSION', '3.6.6');
 
 global $CFG_GLPI;
 
